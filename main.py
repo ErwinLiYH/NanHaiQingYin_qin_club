c0f8c2b3cb6d9134c42d282fa6044773357dfc58
Kylis <1779599839@qq.com>
2020-10-16 21:05:26 +0800

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+import classes
+import pandas
+
+raw_data = pandas.read_excel('93415349_0_迎新选课_79_79.xlsx','Sheet1')
+
+#----------自定义清洗代码----------#
+
+#--------------------------------#
+
+#1.处理同名
+
+#2.处理多次填写
+
+#3.生成全部student对象
+
+#4.生成全部student_combination对象
+
+#5.生成全部lesson对象
